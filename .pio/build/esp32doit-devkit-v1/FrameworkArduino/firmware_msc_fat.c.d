cd308fa2c3d2ce12dc79f3d52e3eff11d7f6967a
TolaVu1 <thanhvu03112002@gmail.com>
2024-10-02 02:45:03 +0700

    commit

---
+++ .pio/build/esp32doit-devkit-v1/FrameworkArduino/firmware_msc_fat.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp32doit-devkit-v1/FrameworkArduino/firmware_msc_fat.c.o: \
+ C:/Users/thanh/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
+ C:/Users/thanh/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
